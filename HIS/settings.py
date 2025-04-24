b937258cc4594f5fb2c1041232197172cb18f6fb
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-24 21:40:30 +0300

    feat: add CSRF trusted origins

--- HIS/settings.py
+++ HIS/settings.py
@@ -105,6 +105,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
