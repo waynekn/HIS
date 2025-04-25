4f0da79c4ba88da531e5c02410c61db5fbc3a4c9
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-25 12:29:20 +0300

    feat: integrate Redux with persistence and logger middleware

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store/store.ts";
 import "./index.css";
 import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ frontend/src/store/store.ts
@@ -0,0 +1,27 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import { rootReducer } from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const persitedReducer = persistReducer(persistConfig, rootReducer);
+
+const isDev = import.meta.env.MODE === "development";
+
+const store = configureStore({
+  reducer: persitedReducer,
+  middleware: (getDefaultMiddleware) =>
+    isDev ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware(),
+});
+
+export const persistor = persistStore(store);
+
+export type StoreDispatch = typeof store.dispatch;
+
+export default store;
