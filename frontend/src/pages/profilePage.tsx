38d6c662ec17453854bbb88fe342bbd2575f350b
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-25 13:10:23 +0300

    feat: add profile page component and update router to include user profile route

---
+++ frontend/src/pages/profilePage.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../store/user/user.selector";
+
+function ProfilePage() {
+  const currentUser = useSelector(selectCurrentUser);
+  return (
+    <div className="min-h-screen">
+      <h1>
+        Dr <span className="font-bold">{currentUser.username}</span>
+      </h1>
+      <ul>
+        <li>
+          <Link to="#">My Clients</Link>
+        </li>
+        <li>
+          <Link to="#">My Programs</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProfilePage;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import LogInForm from "./components/loginForm";
 import SignUpForm from "./components/signUpForm";
+import ProfilePage from "./pages/profilePage";
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route path="login/" element={<LogInForm />} />
         <Route path="signup/" element={<SignUpForm />} />
       </Route>
+      <Route path="user/:username/" element={<ProfilePage />} />
     </Routes>
   );
 }
