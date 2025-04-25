cd1f7881db1a04abd8c3763174eef33350f3561f
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-25 15:06:00 +0300

    feat: create `HealthProgram` model and migrate

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class HealthProgram(models.Model):
+    name = models.CharField(max_length=150, verbose_name='health program name')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.owner.username}'
