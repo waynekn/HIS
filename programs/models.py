a30e335fd3bc136a2951c428bd1c18fea16638c5
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-25 18:37:20 +0300

    feat: add `ProgramEnrollment` model and migration

--- programs/models.py
+++ programs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from clients.models import Client
+
 
 class HealthProgram(models.Model):
     name = models.CharField(max_length=150, verbose_name='health program name')
@@ -8,3 +10,12 @@ class HealthProgram(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.owner.username}'
+
+
+class ProgramEnrollment(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    program = models.ForeignKey(HealthProgram, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.client.name} | program {self.program.name}| doc {self.doctor.username}'
