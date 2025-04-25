5354878c9b86260766ba56de11cf2257489cc432
Wayne Kinyanjui <waynekn24@gmail.com>
2025-04-25 16:59:00 +0300

    feat: create `Client` model and migration

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=150, verbose_name='client name')
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Client {self.name} | Doctor {self.doctor.username}'
